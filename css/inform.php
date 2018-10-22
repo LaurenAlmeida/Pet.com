1d0d2150b80cc89b2b3ef77a3404e15870fdd624
LaurenAlmeida <39421295+LaurenAlmeida@users.noreply.github.com>
2018-10-22 16:22:45 -0300

    Add files via upload

---
+++ css/inform.php
@@ -0,0 +1,52 @@
+<?php
+require_once ("cabecalho.php");
+?>
+<title>Cadastro de Usuário</title>
+<meta charset="utf-8">
+</head>
+<body>
+	<div class="container">
+		<h3 style="text-align: center;">Acidentes domésticos comuns com animais de estimação</h3><br>
+</div>
+	<div class="container">
+    
+<fieldset class="col-md-10">
+<div id="ac1"></div><br><br><p>Ingestão de substâncias toxicas (veneno de rato, produtos de limpeza): mantenha os produtos de limpeza longe dos animais de estimação. Se precisar usar algum tipo de veneno, todo cuidado é pouco com os animais. De preferência, tire-os de casa por 48 horas após a aplicação do produto.</p>
+</fieldset>
+
+</div><br>
+<div class="container">
+	<fieldset class="col-md-10">
+	<div id="ac2"></div><br><br><p>Intoxicação alimentar: esse caso é muito comum durante festas. Os convidados acabam oferecendo restos de alimentos para os animais, que não são acostumados a consumir comidas gorduras, ossos, etc. Logo no começo da festa, oriente sus convidados a não oferecer alimentos para os animais. Fale sobre o quanto isso pode ser prejudicial para saúde deles.</p>
+</fieldset>
+</div><br>
+
+<div class="container">
+<fieldset class="col-md-10">
+<div id="ac3"></div><br><br><p>Ingestão de corpo estranho: esse tipo de acidente é muito comum acontecer com filhotes. Eles pegam meias, brinquedos, objetos pequenos, enfim, tudo quanto objeto que você possa imaginar pode chamar atenção deles. Fique atento! Mantenha sempre ocupados com brinquedos apropriados para cães e gatos.</p></fieldset>
+</div><br>
+
+
+<div class="container">
+	<fieldset class="col-md-10">
+<div id="ac4"></div><br><br><p>Trauma por veículos: esse é outro acidente bastante comum. Muitos animais se escondem ou dormem embaixo do carro e o tutor sem saber que ele está ali acaba atropelando o bichinho. Outra situação comum é ao entrar com o carro na garagem. Muitos confiam que eles vão sair de perto do veículo, mas nem sempre ocorre dessa forma. Mantenha os animais presos sempre que for sair ou entrar da garagem.</p></fieldset>
+</div><br>
+
+<div class="container">
+	<fieldset class="col-md-10">
+<div id="ac5"></div><br><br><p>Trauma de filhotes que acabaram de nascer ou de até 1 mês de idade causado por manuseio de crianças: todo cuidado é pouco com os filhotes. Eles são frágeis e nas mãos das crianças, acidentes são extremamente comuns. Dê preferência, não deixe as crianças muito próximos de filhotes tão pequenos.</p></fieldset>
+</div><br><br>
+<div class="container">
+<p>Existem outros acidentes comuns como lacerações de pele por brigas entre cães, ingestão de medicamentos do tutor, ingestão de drogas como maconha, acidentes relacionados a rede elétricas (Comer fios) e enforcamento por correntes, quando o tutor deixa o animal preso.</p>
+
+<p>Fique atento a esses perigos. Nossos bichinhos são membros da família e eles são como crianças, é preciso estar sempre alerta para evitar acidentes.</p>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php
+require_once("rodape.php")
+?>
